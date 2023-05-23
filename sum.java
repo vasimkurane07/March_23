aab554fa813147ae6fabad2803ec68c332efd0ea
vasim kurane <kuranevasim@gmail.com>
2023-05-23 23:09:03 +0530

    include multiply and sub

--- sum.java
+++ sum.java
@@ -5,5 +5,8 @@ class sum
         int x=3,y=4,z=6,a;
         a=z+x+y;
         System.out.println(""+a);
-    }
+        z=x-y;
+        System.out.println(""+z);
+        x=a*z;
+    } System.out.println(""+x);
 }
